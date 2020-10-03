e602795dafc41c788722f2f36b197355be07736a
Delu08 <fdeluca@fi.uba.ar>
2020-10-02 23:54:45 -0300

    Primer intento todas las funciones

--- tp0.c
+++ tp0.c
@@ -1,23 +1,71 @@
 #include "tp0.h"
 
-/* *****************************************************************
- *                     FUNCIONES A COMPLETAR                       *
- *         (ver en tp0.h la documentación de cada función)         *
- * *****************************************************************/
-
 void swap(int *x, int *y) {
+	int aux = *x;
+	*x = *y;
+	*y = aux;
 }
 
 
 int maximo(int vector[], int n) {
-    return -1;
+	int max = 0;
+	if( n <= 0)
+		return -1;
+
+	for(int i = 1; i < n; i++){
+
+		if(vector[i] > vector[max])
+			max = i;
+
+	}
+
+	return max;
 }
 
 
 int comparar(int vector1[], int n1, int vector2[], int n2) {
-    return 0;
+
+	int i = 0;
+	int j = 0;
+
+	while( (i < n1) && (j < n2) ){
+		if(vector1[i] == vector2[j]){
+			i++;
+			j++;
+		} else if (vector1[i] < vector2[j]){
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+	if( (i != n1) || (j != n2) ){
+		if( i == n1 )
+			return -1;
+
+		if( j == n2)
+			return 1;
+	}
+
+	return 0;
 }
 
 
 void seleccion(int vector[], int n) {
+
+	int min;
+	int aux;
+
+	for(int i = 0; i < n-1; i++){
+		min = i;
+		for(int j = i; j < n; j++){
+			if( vector[j] < vector[min] ){
+				min = j;
+			}
+		}
+
+	aux = vector[i];
+	vector[i] = vector[min];
+	vector[min] = aux;
+	}
 }
