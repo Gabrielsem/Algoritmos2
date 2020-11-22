1a9457c6a45b70cb0ee24d278869499679d3e35e
fdelu <fdeluca@fi.uba.ar>
2020-11-22 13:31:49 -0300

    Agrego cosas para redimensionar

--- hash.c
+++ hash.c
@@ -7,8 +7,9 @@
  * *****************************************************************/
 
 #define CAPACIDAD_INICIAL 10
-#define FACTOR_AGRANDAR 0.7
-#define FACTOR_ACHICAR 0.15
+#define PROP_AGRANDAR 0.7
+#define PROP_ACHICAR 0.15
+#define FACTOR_REDIM 2
 
 enum estados {
 	VACIO,
@@ -38,12 +39,59 @@ struct hash_iter {
  *                        FUNCIONES INTERNAS
  * *****************************************************************/
 
+// Función de hash
+// (djb2, http://www.cse.yorku.ca/~oz/hash.html)
+// Tiene algunas modificaciones para comodidad y
+// para que no tire warnings el compilador.
+size_t hash_func(unsigned char *str, size_t capacidad){
+    size_t hash = 5381;
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + (size_t) c; /* hash * 33 + c */
+
+    return hash % capacidad;
+}
+
 // Inicializa esa cantidad de elementos del vector elementos,
 // poniendo su estado en VACIO.
 void inicializar_elementos(elemento_t* elementos, size_t cantidad){
 	for(int i = 0; i < cantidad; i++){
 		elementos[i].estado = VACIO;
 	}
+}
+
+// Devuelve una nueva capacidad ideal para ese hash de acuerdo a su carga,
+// según la proporción de la cantidad de elementos con la capacidad del hash:
+// - Si es menor a PROP_ACHICAR, devuelve la capacidad actual
+//   del hash dividido FACTOR_REDIM.
+// - Si es mayor a PROP_AGRANDAR, devuelve la capacidad actual
+//   del hash multiplicado por FACTOR_REDIM.
+// - Sino, devuelve la capacidad actual del hash.
+size_t nueva_capacidad(const hash_t* hash){
+	size_t capacidad = hash->capacidad;
+	float proporcion = hash->cantidad / hash->capacidad;
+
+	if(proporcion < PROP_ACHICAR){
+		capacidad = hash->capacidad/FACTOR_REDIM;
+		if(capacidad < CAPACIDAD_INICIAL)
+			capacidad = CAPACIDAD_INICIAL;
+	}
+
+	if(proporcion > PROP_AGRANDAR){
+		capacidad = hash->capacidad * FACTOR_REDIM;
+	}
+
+	return capacidad;
+}
+
+bool redim_hash(hash_t* hash){
+	size_t nueva_cap = nueva_capacidad(hash);
+	if(nueva_cap == hash->capacidad)
+		return true;
+
+	//hago cosas
+	return true;
 }
 
 /* ******************************************************************
