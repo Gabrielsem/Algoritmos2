cb1708c286f87c4d790dfa83a40e991ea8c53206
fdelu <fdeluca@fi.uba.ar>
2020-11-24 21:22:41 -0300

    Saco <stdio.h> que quedo de debug con printfs

--- hash.c
+++ hash.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <stdio.h>
 
 /* ******************************************************************
  *                  DEFINICIÓN DE LAS ESTRUCTURAS
