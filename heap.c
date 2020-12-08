16346f525db62a87c74eda0b5f376099784e2dac
Gabrielsem <gsemorile@fi.uba.ar>
2020-12-08 01:03:43 -0300

    algunas prim

--- heap.c
+++ heap.c
@@ -39,13 +39,25 @@ void downheap(void** datos, size_t n, cmp_func_t cmp) {
 	return;
 }
 
+bool heap_redim(heap_t *heap){
+	return false;
+}
+
 /* ******************************************************************
  *                  		PRIMITIVAS HEAP
  * *****************************************************************/
 
 heap_t *heap_crear(cmp_func_t cmp) {
-	heap_t* tmp = NULL;
-	return tmp;
+	heap_t* heap = malloc(sizeof(heap_t));
+	if(!heap) return NULL;
+	heap->cap = 10;
+	heap->cant = 0;
+	heap->datos = malloc(sizeof(void*) * heap->cap);
+	if(!heap->datos) {
+		free(heap);
+		return NULL;
+	}
+	return heap;
 }
 
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
@@ -54,23 +66,32 @@ heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
 }
 
 void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e)) {
-	return;
+	if(destruir_elemento) {
+		for(size_t i = 0; i < heap->cant; i++) {
+			destruir_elemento(heap->datos[i]);
+		}
+	}
+	free(heap->datos);
+	free(heap);
 }
 
 size_t heap_cantidad(const heap_t *heap) {
-	return 0;
+	return heap->cant;
 }
 
 bool heap_esta_vacio(const heap_t *heap) {	
-	return false;
+	return heap->cant ? false : true;
 }
 
 bool heap_encolar(heap_t *heap, void *elem) {
+	if(!heap_redim(heap)) return false;
+
+
 	return false;
 }
 
 void *heap_ver_max(const heap_t *heap) {
-	return NULL;
+	return heap->datos[heap->cant - 1];
 }
 
 void *heap_desencolar(heap_t *heap) {
