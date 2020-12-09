fea6ff8b151b1d9676079206cefe195deb22f645
Gabrielsem <gsemorile@fi.uba.ar>
2020-12-08 22:41:00 -0300

    arreglos

--- heap.c
+++ heap.c
@@ -7,8 +7,7 @@
  * *****************************************************************/
 
 #define CAPACIDAD_INICIAL 10
-#define PORC_AGRANDAR 70
-#define PORC_ACHICAR 15
+#define PORC_ACHICAR 25
 #define FACTOR_REDIM 2
 
 struct heap {
@@ -45,12 +44,12 @@ void downheap(void** datos, size_t n, size_t tam, cmp_func_t cmp) {
 	return;
 }
 
-size_t nueva_cap(const heap_t* heap){
+size_t nueva_cap(const heap_t* heap) {
 	size_t cap = heap->cap;
-	size_t porcentaje =  heap->cant * 100 / heap->cap;
+	size_t porcentaje =  heap->cant*100/heap->cap;
 
 	if(porcentaje < PORC_ACHICAR){
-		cap = heap->cap / FACTOR_REDIM;
+		cap = heap->cap/FACTOR_REDIM;
 		if(cap < CAPACIDAD_INICIAL)
 			cap = CAPACIDAD_INICIAL;
 	}
@@ -62,11 +61,13 @@ size_t nueva_cap(const heap_t* heap){
 	return cap;
 }
 
-bool heap_redim(heap_t *heap){
-	size_t cap = nueva_cap(heap);
-	if(cap == heap->cap) return true;
-
-	void** datos_nuevo = realloc(heap->datos, cap * sizeof(void*));
+bool heap_redim(heap_t *heap) {
+	size_t cap = heap->cap;
+	size_t porcentaje =  heap->cant*100/heap->cap;
+	if(cap == heap->cant) cap*=FACTOR_REDIM;
+	else if(porcentaje < PORC_ACHICAR) cap/=FACTOR_REDIM;
+	else return true;
+	void** datos_nuevo = realloc(heap->datos,cap * sizeof(void*));
 	if(datos_nuevo == NULL) return false;
 	heap->datos = datos_nuevo;
 	heap->cap = cap;
@@ -110,7 +111,7 @@ size_t heap_cantidad(const heap_t *heap) {
 }
 
 bool heap_esta_vacio(const heap_t *heap) {	
-	return !heap->cant;
+	return heap->cant ? false : true;
 }
 
 bool heap_encolar(heap_t *heap, void *elem) {
@@ -122,15 +123,17 @@ bool heap_encolar(heap_t *heap, void *elem) {
 }
 
 void *heap_ver_max(const heap_t *heap) {
+	if(heap_esta_vacio(heap)) return NULL;
 	return heap->datos[0];
 }
 
 void *heap_desencolar(heap_t *heap) {
-	if(!heap_redim(heap)) return NULL;
+	if(heap_esta_vacio(heap)) return NULL;
+	heap_redim(heap);
 	void* dato = heap->datos[0];
 	heap->cant -= 1;
 	heap->datos[0] = heap->datos[heap->cant];
-	downheap(heap->datos, (size_t)0, heap->cant, heap->cmp);
+	downheap(heap->datos, 0, heap->cant, heap->cmp);
 	return dato;
 }
 
