47b64700aed0e3459047a960f6390541c400aedc
Gabrielsem <gsemorile@fi.uba.ar>
2020-12-09 22:56:49 -0300

    pruebas listas

--- heap_pruebas.c
+++ heap_pruebas.c
@@ -1,15 +1,22 @@
 #include "testing.h"
 #include "heap.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+int cmp_int(const void* int_1, const void* int_2){
+	const int* a = int_1;
+	const int* b = int_2;
+	return (*a - *b);
+}	
 void prueba_heap_vacio() {
-	heap_t* heap = heap_crear(strcmp);
+	heap_t* heap = heap_crear(cmp_int);
 	if(!heap) return;
 	print_test("se creo heap y esta vacio",heap_esta_vacio(heap) && (heap_cantidad(heap) == 0));
 	print_test("desencolar devuelve NULL",!heap_desencolar(heap));
 	heap_destruir(heap,NULL);
 }
 void prueba_heap_encolar() {
-	heap_t* heap = heap_crear(strcmp);
+	heap_t* heap = heap_crear(cmp_int);
 	if(!heap) return;
 	for(size_t i=0; i<9; i++) {
 		heap_encolar(heap,(void*)i);
@@ -24,7 +31,7 @@ void prueba_heap_encolar() {
 }
 
 void prueba_heap_desencolar() {
-	heap_t* heap = heap_crear(strcmp);
+	heap_t* heap = heap_crear(cmp_int);
 	if(!heap) return;
 	for(size_t i=0; i<9; i++) {
 		heap_encolar(heap,(void*)i);
@@ -41,9 +48,24 @@ void prueba_heap_desencolar() {
 	heap_destruir(heap,NULL);
 }
 
+void prueba_heap_destruir() {
+	heap_t* heap = heap_crear(cmp_int);
+	if(!heap) return;
+	int* a = malloc(sizeof(int));
+	int* b = malloc(sizeof(int));
+	int* c = malloc(sizeof(int));
+	int* d = malloc(sizeof(int));
+	heap_encolar(heap,(void*)a);
+	heap_encolar(heap,(void*)b);
+	heap_encolar(heap,(void*)c);
+	heap_encolar(heap,(void*)d);
+	heap_destruir(heap,free);
+}
+
 void pruebas_heap_estudiante(void){
 	prueba_heap_vacio();
 	prueba_heap_encolar();
 	prueba_heap_desencolar();
+	prueba_heap_destruir();
 	return;
 }
