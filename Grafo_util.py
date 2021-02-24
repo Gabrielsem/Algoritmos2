a5b38bd15661033889dde48b67bd0fa2e8ed85fa
fdelu <fdeluca@fi.uba.ar>
2021-02-24 09:35:16 -0300

    Recorrido BFS

--- Grafo_util.py
+++ Grafo_util.py
@@ -1,1 +1,30 @@
 from Grafo import Grafo
+from collections import deque
+
+# Hace un recorrido BFS desde el vértice de origen indicado. 
+# Devuelve los diccionarios de padres y ordenes, en ese orden
+# Si se indica un vértice de corte, se corta al encontrar ese vértice.
+# Si se indica orden de corte, se recorren todos los vértices hasta ese orden inclusive.
+def recorrido_bfs(grafo, v_origen, v_corte = None, orden_corte = float("inf")):
+	cola = deque([v_origen])
+	padres = {v_origen:None}
+	ordenes = {v_origen:0}
+	if v_origen == v_corte:
+		return padres, ordenes
+
+	while(len(cola) > 0):
+		v = cola.pop()
+		for w in grafo.adyacentes(v):
+			if w in ordenes:
+				continue
+			padres[w] = v
+			ordenes[w] = ordenes[v] + 1
+
+			if w == v_corte:
+				return padres, ordenes
+
+			if ordenes[w] < orden_corte:
+				cola.appendleft(w)
+
+	return padres, ordenes
+
