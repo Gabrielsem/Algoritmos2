269c28d5b83a2055bdf4f9b01c3e45a1ddc9b66e
fdelu <fdeluca@fi.uba.ar>
2021-02-27 01:49:13 -0300

    Adapto la carga de datos para armar el grafo de canciones similares solo si se necesita

--- Recomendify.py
+++ Recomendify.py
@@ -1,6 +1,6 @@
 from Grafo import Grafo
 import Grafo_util
-from lectura_datos import cargar_datos
+from lectura_datos import leer_archivo
 import sys
 
 def main():
@@ -10,7 +10,7 @@ def main():
 
 	usuarios_gustos = Grafo() # Grafo bipartito entre usuarios y canciones, conectándo a los usuarios con las canciones que les gustan
 	canciones_similares = Grafo() # Grafo de canciones cuyas aristas conectan canciones similares
-	cargar_datos(sys.argv[1], usuarios_gustos, canciones_similares)
+	leer_archivo(sys.argv[1], usuarios_gustos, canciones_similares)
 
 if __name__ == "__main__":
 	main()
--- lectura_datos.py
+++ lectura_datos.py
@@ -2,22 +2,23 @@ import csv
 from Grafo import Grafo
 
 
-def agregar_gusto(usuarios_gustos, playlist_nombre, usuario, cancion):
+def agregar_grafo_usuarios(usuarios_gustos, playlist_nombre, usuario, cancion):
 	usuarios_gustos.agregar_vertice(usuario)
 	usuarios_gustos.agregar_vertice(cancion)
 	usuarios_gustos.agregar_arista(usuario, cancion, peso = playlist_nombre)
 
-def agregar_similar(canciones_similares, playlists, playlist_actual, cancion_actual):
-	canciones_similares.agregar_vertice(cancion_actual)
-
+def agregar_dic_playlists(canciones_similares, playlists, playlist_actual, cancion_actual):
 	if playlist_actual not in playlists:
 		playlists[playlist_actual] = [cancion_actual]
 	else:
-		for cancion_anterior in playlists[playlist_actual]:
-			canciones_similares.agregar_arista(cancion_anterior, cancion_actual)
 		playlists[playlist_actual].append(cancion_actual)
 
-def cargar_datos(ruta_archivo, usuarios_gustos, canciones_similares):
+# Devuelve tupla:
+# 1. Grafo bipartito de usuarios y canciones, con aristas entre los usuarios y las canciones
+# que esten en sus playlists
+# 2. Diccionario de playlists (Clave = nombre de playlist, dato = lista de todas las canciones)
+# Ese diccionario se puede pasar a grafo_canciones() para crear el Grafo correspondiente.
+def leer_archivo(ruta_archivo, usuarios_gustos, canciones_similares):
 	playlists = {} # Diccionario de playlists, guarda listas con las canciones de la playlist
 
 	with open(ruta_archivo, "r") as archivo:
@@ -25,5 +26,19 @@ def cargar_datos(ruta_archivo, usuarios_gustos, canciones_similares):
 		for fila in lector:
 			cancion = fila["TRACK_NAME"] + " - " + fila["ARTIST"]
 
-			agregar_gusto(usuarios_gustos, fila["PLAYLIST_NAME"], fila["USER_ID"], cancion)
-			agregar_similar(canciones_similares, playlists, fila["PLAYLIST_ID"], cancion)
+			agregar_grafo_usuarios(usuarios_gustos, fila["PLAYLIST_NAME"], fila["USER_ID"], cancion)
+			agregar_dic_playlists(canciones_similares, playlists, fila["PLAYLIST_ID"], cancion)
+
+# Devuelve un grafo con vértices de canciones y aristas entre canciones que esten en una misma playlist.
+# Recibe diccionario de playlists (Clave = nombre de playlist, dato = lista de todas las canciones)
+def grafo_canciones(playlists):
+	grafo = Grafo()
+	for playlist in playlists:
+		agregados = 0
+		for cancion in playlist:
+			grafo.agregar_vertice(cancion)
+			agregados += 1
+			for i in range(0, agregados - 1):
+				grafo.agregar_arista(playlist[i], cancion)
+
+
