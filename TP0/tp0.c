8f8a9c7cc7c0d11adff1ca7cacae87a218e00301
fdelu <fdeluca@fi.uba.ar>
2021-04-20 23:28:40 -0300

    Emprolijamiento código TP0

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -10,14 +10,12 @@ void swap(int *x, int *y) {
 int maximo(int vector[], int n) {
 	int max = 0;
 
-	if(n <= 0)
+	if (n <= 0)
 		return -1;
 
-	for(int i = 1; i < n; i++){
-
-		if(vector[i] > vector[max])
+	for (int i = 1; i < n; i++) {
+		if (vector[i] > vector[max])
 			max = i;
-
 	}
 
 	return max;
@@ -28,8 +26,8 @@ int comparar(int vector1[], int n1, int vector2[], int n2) {
 
 	int i = 0;
 
-	while( (i < n1) && (i < n2) ){
-		if(vector1[i] == vector2[i]){
+	while ( (i < n1) && (i < n2) ) {
+		if (vector1[i] == vector2[i]){
 			i++;
 		} else if (vector1[i] < vector2[i]){
 			return -1;
@@ -38,21 +36,20 @@ int comparar(int vector1[], int n1, int vector2[], int n2) {
 		}
 	}
 
-	if( (i == n1) && (i != n2) )
+	if ( (i == n1) && (i != n2) )
 		return -1;
 
-	if( (i != n1) && (i == n2) )
+	if ( (i != n1) && (i == n2) )
 		return 1;
 
 	return 0;
 }
 
 void seleccion(int vector[], int n) {
-	for(int i = 0; i < n-1; i++){
+	for (int i = 0; i < n-1; i++) {
 		
 		int max = maximo(vector, n-i);
-
-		swap(vector+n-1-i, vector+max);
+		swap(vector+ n - 1 - i, vector + max);
 
 	}
 }
